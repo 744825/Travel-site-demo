abcb8c7e4534629f1ad0a8ed6381af4bd5e82c90
Karan <shetekaran50@gmail.com>
2020-09-11 02:45:20 +0530

    Updated cloud function with password

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,28 @@
 exports.handler = function(event, context, callback){
+    const secretContent = `
+    <h3>Welcome to the secret area</h3>
+    <p> Here we can tell you that skt is bule that is <strong>BLUE</strong></p>`
+    
+    let body
+    
+    if(event.body){
+        body =JSON.parse()
+    } else{
+        body = {}
+    }
+
+    if (body.password == "javascript") {
+        
     callback(null, {
         statusCode: 200,
-        body: "Welcome to super secret area"
+        body: secretContent
     })
+    } else {
+        callback(null, {
+            statusCode: 401
+        })
+    }
+
+
+
 }
